2d5bb8a5e329b28cce5cb189256f2d59d25f5a82
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-05 17:08:30 +0200

    [UPD] Created a table to contain all the data

--- index.php
+++ index.php
@@ -56,13 +56,28 @@
         <div class="row">
             <div class="col-12">
                 <h1>Tutti gli hotel</h1>
-                <ul>
-                    <li><?php var_dump($hotels[0]);?></li>
-                    <li><?php var_dump($hotels[1]);?></li>
-                    <li><?php var_dump($hotels[2]);?></li>
-                    <li><?php var_dump($hotels[3]);?></li>
-                    <li><?php var_dump($hotels[4]);?></li>
-                </ul>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">name</th>
+                        <th scope="col">description</th>
+                        <th scope="col">parking</th>
+                        <th scope="col">vote</th>
+                        <th scope="col">distance to center</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($hotels as $hotel): ?>
+                        <tr>
+                            <td><?= $hotel['name'];  ?></td>
+                            <td><?= $hotel['description']; ?></td>
+                            <td><?= $hotel['parking'] ? 'Sì' : 'No' ;?></td>
+                            <td><?= $hotel['vote'];?>/5</td>
+                            <td><?= $hotel['distance_to_center']; ?> km</td>
+                        </tr>
+                        <?php endforeach; ?> 
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
